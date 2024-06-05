ba68ca0b0844ddd906680c6800ce9062c7bf0353
emmanuel jacob maldonado lopez <jacobmaldonado99@gmail.com>
2024-06-04 21:11:55 -0600

    fix

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def get_driver():
     from webdriver_manager.chrome import ChromeDriverManager
     service = ChromeService(executable_path=ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--remote-debugging-port=9222')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0')
     #chrome_options.headless = True
     if sys.platform == "win32":
@@ -66,14 +66,13 @@ def check_messages() -> None:
             messages_to_reply = list(filter(lambda x: x['message'] == "1" or x['message'] == "2" or x['message'] == "3" , messages))
             print(messages_to_reply)
             for message in messages_to_reply:
-                pass
-                #messenger.find_user(message["sender"].replace("+", "").replace(" ", ""))
-                # if message['message'] == "1":
-                #     send_message(driver, template_aceptado())
-                # elif message['message'] == "2":
-                #     send_message(driver, template_cancelado())
-                # elif message['message'] == "3":
-                #     send_message(driver, template_modificar())
+                messenger.find_user(message["sender"].replace("+", "").replace(" ", ""))
+                if message['message'] == "1":
+                    send_message(driver, template_aceptado())
+                elif message['message'] == "2":
+                    send_message(driver, template_cancelado())
+                elif message['message'] == "3":
+                    send_message(driver, template_modificar())
         except Exception as bug:
             print(bug)
     else:
